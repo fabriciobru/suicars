7a0e674c9313763528a40a4fd8125df4fa0363c1
fabriciobru <fabriciobruno895@gmail.com>
2025-09-03 17:58:14 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,59 @@
+// src/config/nftAssets.js (ou diretamente no seu componente App.js)
+
+export const boards = [
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20common.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20common%202.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20common%203.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20common%204.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20common%205.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20hybrid.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20hybrid%202.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20hybrid%203.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20electric.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20electric%202.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeid3oaxluzmwiakkpcq47zsaqmip7ctpvqebdfmn2jmeq4iwdsnpfy/board%20electric%203.png',
+  // ... adicione todas as URLs de seus boards
+];
+
+export const colors = [
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%201.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%202.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%203.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%204.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%205.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%206.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeih7i3igoih7uqz4tvuepivhsdph4avk7krpis745lvz3gmomplnbi/fundo%207.png',
+  // ... adicione todas as URLs de suas cores
+];
+
+export const cars = [
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%201.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/Design%20sem%20nome.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%202.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%203.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%204.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%205.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%206.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%207.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%208.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%209.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2010.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2011.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2012.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2013.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2014.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2015.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2016.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2017.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2018.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2019.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2020.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2021.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2022.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2023.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2024.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2026.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2027.png',
+  'https://green-quick-ape-954.mypinata.cloud/ipfs/bafybeif36ypml7s3dgr2pnaptavqmd45zmfazsvw4l6cqsbj5feesleo6a/carro%2028.png',
+  // ... adicione todas as URLs de seus carros
+];
